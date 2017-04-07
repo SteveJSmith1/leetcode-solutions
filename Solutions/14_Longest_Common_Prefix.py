a1c44ea25d94a3331adc805b299c7dcc696a5b7a
Steve Smith <stevejsmith567@gmail.com>
2017-04-07 11:50:46 +0100

    Input functional tests written

--- Solutions/14_Longest_Common_Prefix.py
+++ Solutions/14_Longest_Common_Prefix.py
@@ -17,7 +17,29 @@ class Solution(object):
         import os
         return os.path.commonprefix(strs)
 
-strs = ["test", "testy", "testing", "testicles"]
+
+
+from nose.tools import assert_equal, assert_raises
+
+
+class TestLongestCommonPrefix(object):
+
+    def test_longestCommonPrefix(self):
+        solution = Solution()
+        assert_raises(TypeError, solution.longestCommonPrefix, None)
+        assert_raises(TypeError, solution.longestCommonPrefix, 3)
+        assert_equal(solution.longestCommonPrefix(\
+                                                  ["test", "testy", "testing",\
+                                                   "testicles"]),  'test')
         
-test = Solution()
-test.longestCommonPrefix(strs)
+        print('Success: test_searchInsert')
+
+
+def main():
+    test = TestLongestCommonPrefix()
+    test.test_longestCommonPrefix()
+
+
+if __name__ == '__main__':
+    main()
+

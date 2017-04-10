e92a5a8ad1cc4c608740956292ae23e8c3bf23c3
Steve Smith <stevejsmith567@gmail.com>
2017-04-10 13:32:34 +0100

    Functional tests pass

---
+++ WIP/168_Excel_Sheet_Column_Title.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 10 12:26:17 2017
+
+@author: SteveJSmith1
+"""
+class Solution(object):
+    def convertToTitle(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        title = ''
+        alist = 'ABCDEFGHIJKLMNOPQRSTUVWXY'
+        while n:
+            mod = (n-1) % 26
+            n = int((n - mod) / 26)  
+            title += alist[mod]
+        return title[::-1]
+
+from nose.tools import assert_equal
+
+
+class TestConvertToTitle(object):
+
+    def test_convertToTitle(self):
+        solution = Solution()
+        
+        assert_equal(solution.convertToTitle(62354), "CNFF")
+        assert_equal(solution.convertToTitle(367), "NC")
+        assert_equal(solution.convertToTitle(12), "L")
+        
+        print('Success: test_convertToTitle')
+
+
+def main():
+    test = TestConvertToTitle()
+    test.test_convertToTitle()
+
+
+if __name__ == '__main__':
+    main()
+       
+ 
+ 

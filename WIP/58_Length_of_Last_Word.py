c8938922c759241a4570f1cc228e81c95626d532
Steve Smith <stevejsmith567@gmail.com>
2017-04-10 10:32:27 +0100

    Functional tests pass

--- WIP/58_Length_of_Last_Word.py
+++ WIP/58_Length_of_Last_Word.py
@@ -8,15 +8,23 @@ Created on Mon Apr 10 10:15:47 2017
 class Solution(object):
     def lengthOfLastWord(self, s):
         """
+        Returns length of last word in a string if it exists
+        
         :type s: str
         :rtype: int
         """
-        return
         
+        try:
+            words = s.split()
+            return len(words[-1])
+        except:
+            return 0
+    
+    
 
 
 
-from nose.tools import assert_equal, assert_raises
+from nose.tools import assert_equal
 
 
 class TestLengthOfLastWord(object):
@@ -27,7 +35,11 @@ class TestLengthOfLastWord(object):
         assert_equal(solution.lengthOfLastWord("Hello World"), 5)
         assert_equal(solution.lengthOfLastWord(""), 0)
         assert_equal(solution.lengthOfLastWord(" "), 0)
-        assert_raises(TypeError, solution.lengthOfLastWord, None)
+        assert_equal(solution.lengthOfLastWord("Hello World "), 5)
+        assert_equal(solution.lengthOfLastWord("testing blank at end "), 3)
+        
+        
+        
         
         
         

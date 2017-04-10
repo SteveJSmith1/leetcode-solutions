41af5bac7968b222ab03fcfd28485fb467ccb924
Steve Smith <stevejsmith567@gmail.com>
2017-04-10 12:14:36 +0100

    Functional tests written

---
+++ WIP/125_Valid_Palindrome.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 10 12:11:27 2017
+
+@author: SteveJSmith1
+"""
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        Given a string, determine if it is a palindrome, 
+        considering only alphanumeric characters and ignoring cases.
+        
+        :type s: str
+        :rtype: bool
+        """
+        return
+    
+from nose.tools import assert_equal
+
+
+class TestIsPalindrome(object):
+
+    def test_isPalindrome(self):
+        solution = Solution()
+        
+        assert_equal(solution.isPalindrome("A man, a plan, a canal: Panama"), True)
+        assert_equal(solution.isPalindrome(""), True)
+        assert_equal(solution.isPalindrome("race a car"), True)
+               
+        
+        print('Success: test_isPalindrome')
+
+
+def main():
+    test = TestIsPalindrome()
+    test.test_isPalindrome()
+
+
+if __name__ == '__main__':
+    main()
+       
+ 

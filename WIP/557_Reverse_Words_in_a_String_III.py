9f0673752e5f76d18f31ebf66a493a9380b91ac4
Steve Smith <stevejsmith567@gmail.com>
2017-04-12 12:17:57 +0100

    Functional tests written

---
+++ WIP/557_Reverse_Words_in_a_String_III.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 12:11:55 2017
+
+@author: SteveJSmith1
+"""
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        Given a string, you need to reverse the order of characters in each 
+        word within a sentence while still preserving whitespace and initial 
+        word order.
+        
+        :type s: str
+        :rtype: str
+        """
+        return
+    
+from nose.tools import assert_equal
+
+
+class TestReverseWords(object):
+
+    def test_reverseWords(self):
+        solution = Solution()
+        
+        assert_equal(solution.reverseWords("Let's take LeetCode contest"),
+                     "s'teL ekat edoCteeL tsetnoc")
+        assert_equal(solution.reverseWords("ChiPs' and, ChEeSe"),
+                     "'sPihC ,dna eSeEhC")
+        assert_equal(solution.reverseWords("TuR'Nip"), "piN'RuT")
+
+        print('Success: test_reverseWords')
+
+
+def main():
+    test = TestReverseWords()
+    test.test_reverseWords()
+
+
+if __name__ == '__main__':
+    main()
+       

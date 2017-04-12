556b74dd0fc693b13cd5c627853aec33e501443b
Steve Smith <stevejsmith567@gmail.com>
2017-04-12 15:24:59 +0100

    New functional test written which passes
    Exception on submission dealt with

--- WIP/448_Find_All_Numbers_Disappeared_in_an_Array.py
+++ WIP/448_Find_All_Numbers_Disappeared_in_an_Array.py
@@ -28,6 +28,9 @@ class Solution(object):
         :type nums: List[int] 
         :rtype: List[int] 
         """ 
+        if len(nums) == 0:
+            return []
+        
         snums = sorted(set(nums))
 
         for i in range(1, snums[-1] + 1):
@@ -47,7 +50,8 @@ class TestFindDisappearedNumbers(object):
         solution = Solution() 
                  
         assert_equal(solution.findDisappearedNumbers([4,3,2,7,8,2,3,1]), [5, 6]) 
-        assert_equal(solution.findDisappearedNumbers([1,4,2,2,5,6]), [3]) 
+        assert_equal(solution.findDisappearedNumbers([1,4,2,2,5,6]), [3])
+        assert_equal(solution.findDisappearedNumbers([]), []) 
          
         print('Success: test_findDisappearedNumbers') 
  

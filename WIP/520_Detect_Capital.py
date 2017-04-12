0d2b7f042f4e808542aa6db8c08bbaf1e22d9cf1
Steve Smith <stevejsmith567@gmail.com>
2017-04-12 15:44:00 +0100

    Functional tests written

---
+++ WIP/520_Detect_Capital.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 15:40:46 2017
+
+@author: SteveJSmith1
+"""
+
+class Solution(object):
+    def detectCapitalUse(self, word):
+        """
+        Given a word, you need to judge whether the usage of capitals in 
+        it is right or not.
+
+        We define the usage of capitals in a word to be right when one of 
+        the following cases holds:
+
+            All letters in this word are capitals, like "USA".
+            All letters in this word are not capitals, like "leetcode".
+            Only the first letter in this word is capital if it has more than 
+            one letter, like "Google".
+        
+        Otherwise, we define that this word doesn't use capitals in a right way.
+        :type word: str
+        :rtype: bool
+        """
+        return
+    
+from nose.tools import assert_equal
+
+
+class TestDetectCapitalUse(object):
+
+    def test_detectCapitalUse(self):
+        solution = Solution()
+        
+        assert_equal(solution.detectCapitalUse("USA"), True)
+        assert_equal(solution.detectCapitalUse("FlaG"), False)
+        assert_equal(solution.detectCapitalUse("Chips"), True)
+       
+        print('Success: test_detectCapitalUse')
+
+
+def main():
+    test = TestDetectCapitalUse()
+    test.test_detectCapitalUse()
+
+
+if __name__ == '__main__':
+    main()
+       
+ 

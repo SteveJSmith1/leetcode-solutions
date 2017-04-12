b52ab471c2e9a395f5fc429c5435e5c5fff2e405
Steve Smith <stevejsmith567@gmail.com>
2017-04-12 13:42:32 +0100

    Functional tests written

---
+++ WIP/496_Next_Greater_Element_I.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 13:37:13 2017
+
+@author: SteveJSmith1
+"""
+
+class Solution(object):
+    def nextGreaterElement(self, findNums, nums):
+        """
+        You are given two arrays (without duplicates) nums1 and nums2 
+        where nums1’s elements are subset of nums2. 
+        Find all the next greater numbers for nums1's elements in the 
+        corresponding places of nums2.
+
+        The Next Greater Number of a number x in nums1 is the first 
+        greater number to its right in nums2. 
+        If it does not exist, output -1 for this number.
+        
+        :type findNums: List[int]
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        return
+    
+from nose.tools import assert_equal
+
+
+class TestNextGreaterElement(object):
+
+    def test_nextGreaterElement(self):
+        solution = Solution()
+        
+        assert_equal(solution.nextGreaterElement([4,1,2], [1,3,4,2]), 
+                     [-1,3,-1])
+        assert_equal(solution.nextGreaterElement([2,4], [1,2,3,4]),
+                     [3,-1])
+        assert_equal(solution.nextGreaterElement([4,1,2,3,5],[1,3,4,2, 5, 6, 7]),
+                     [5,3,5,4,6])
+        
+        
+        print('Success: test_nextGreaterElement')
+
+
+def main():
+    test = TestNextGreaterElement()
+    test.test_nextGreaterElement()
+
+
+if __name__ == '__main__':
+    main()
+       
+ 
+ 

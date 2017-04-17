e6f2b25d876217486b8841b8b02e947120eec45a
Steve Smith <stevejsmith567@gmail.com>
2017-04-17 12:02:13 +0100

    Functional tests written

---
+++ WIP/383_Ransom_Note.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 17 11:58:19 2017
+
+@author: SteveJSmith1
+
+Given an arbitrary ransom note string and another string containing 
+letters from all the magazines, write a function that will return true 
+if the ransom note can be constructed from the magazines ; otherwise, 
+it will return false.
+
+Each letter in the magazine string can only be used once in your ransom note.
+
+Note:
+You may assume that both strings contain only lowercase letters.
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+
+"""
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        return 
+    
+from nose.tools import assert_equal
+
+
+class TestCanConstruct(object):
+
+    def test_canConstruct(self):
+        solution = Solution()
+        
+        assert_equal(solution.canConstruct("a", "b"), False)
+        assert_equal(solution.canConstruct("aa", "ab"), False)
+        assert_equal(solution.canConstruct("aa", "aab"), True)
+        assert_equal(solution.canConstruct("ransom note","sansomr tone"), True)
+        
+        print('Success: test_canConstruct')
+
+
+def main():
+    test = TestCanConstruct()
+    test.test_canConstruct()
+
+
+if __name__ == '__main__':
+    main()
+       
+ 

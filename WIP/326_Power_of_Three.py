d67b1f7049b23c619332d68a1cfb39d579b31372
Steve Smith <stevejsmith567@gmail.com>
2017-04-18 13:31:45 +0100

    Functional tests pass

---
+++ WIP/326_Power_of_Three.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 18 13:22:17 2017
+
+@author: SteveJSmith1
+
+Given an integer, write a function to determine if it is a power of three.
+"""
+
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n == 1:
+            return True
+        elif n == 0:
+            return False
+        
+        return divmod(n, 3)[-1] == 0
+    
+
+from nose.tools import assert_equal
+
+
+class TestIsPowerOfThree(object):
+
+    def test_isPowerOfThree(self):
+        solution = Solution()
+        
+        assert_equal(solution.isPowerOfThree(27), True)
+        assert_equal(solution.isPowerOfThree(1), True)
+        assert_equal(solution.isPowerOfThree(0), False)
+        assert_equal(solution.isPowerOfThree(11), False)
+        print('Success: test_isPowerOfThree')
+
+
+def main():
+    test = TestIsPowerOfThree()
+    test.test_isPowerOfThree()
+
+
+if __name__ == '__main__':
+    main()
+       
+ 

ba42765bf07d242ec224c9bc3dbc4d8c3c7a7571
Steve Smith <stevejsmith567@gmail.com>
2017-04-18 10:15:21 +0100

    Functional tests pass

--- WIP/258_Add_Digits.py
+++ WIP/258_Add_Digits.py
@@ -19,9 +19,17 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        return
+        listnum = list(str(num))
+        n = 10
+        # addDigits total will be 9 or less
+        while n > 9:
+            # convert to integers and sum
+            n = sum([int(v) for v in listnum])
+            # change integer to list of digits
+            listnum = list(str(n))
+        return n
 
-    
+  
 from nose.tools import assert_equal
 
 
@@ -31,7 +39,7 @@ class TestAddDigits(object):
         solution = Solution()
         
         assert_equal(solution.addDigits(38), 2)
-        assert_equal(solution.addDigits(0), 2)
+        assert_equal(solution.addDigits(0), 0)
         assert_equal(solution.addDigits(345678), 6)
         assert_equal(solution.addDigits(1234567890), 9)
         

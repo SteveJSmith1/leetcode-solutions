4a7132b7f314274b1bab9af2ca93bb4cade99848
Steve Smith <stevejsmith567@gmail.com>
2017-04-18 13:58:07 +0100

    Functional tests written

---
+++ WIP/231_Power_of_Two.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 18 13:55:31 2017
+
+@author: SteveJSmith1
+Given an integer, write a function to determine if it is a power of two.
+"""
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        
+        if a number is a power of two then in it's binary
+        form, there is only one 1
+        """
+        return
+
+
+from nose.tools import assert_equal
+
+
+class TestIsPowerOfTwo(object):
+
+    def test_isPowerOfTwo(self):
+        solution = Solution()
+        
+        assert_equal(solution.isPowerOfTwo(1), True)
+        assert_equal(solution.isPowerOfTwo(0), False)
+        assert_equal(solution.isPowerOfTwo(-4), False)
+        assert_equal(solution.isPowerOfTwo(4), True)
+        assert_equal(solution.isPowerOfTwo(1025), False)
+        print('Success: test_isPowerOfTwo')
+
+
+def main():
+    test = TestIsPowerOfTwo()
+    test.test_isPowerOfTwo()
+
+
+if __name__ == '__main__':
+    main()
+       
+ 
